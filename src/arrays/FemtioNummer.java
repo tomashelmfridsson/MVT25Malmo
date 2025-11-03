5d24d3ebb131f293e622b18e5158f906df1371df
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2025-11-03 11:24:16 +0100

    Uppgift skapa 50 tals array, nu med 1-50

--- src/arrays/FemtioNummer.java
+++ src/arrays/FemtioNummer.java
@@ -13,7 +13,7 @@ public class FemtioNummer {
         // [plats] = värde
         nbrArr[0] = 1;   // startvärde annars har jag inget att dubbla senare
         for (int i = 1; i < 50; i++) {
-            nbrArr[i] = nbrArr[i-1] * 2; // Dubblar föregåendes siffra
+            nbrArr[i] = nbrArr[i - 1] * 2; // Dubblar föregåendes siffra
         }
         System.out.println(nbrArr);
         for (int i = 0; i < nbrArr.length; i++) {
@@ -21,18 +21,21 @@ public class FemtioNummer {
         }
 
         int[] nbrArrRandom = new int[50];
-        for (int i=0; i< nbrArrRandom.length;i++){
-            nbrArrRandom[i] = (int) ((Math.random()-0.5)*200);
+        for (int i = 0; i < nbrArrRandom.length; i++) {
+            nbrArrRandom[i] = (int) ((Math.random() - 0.5) * 200);
         }
         for (int i = 0; i < nbrArrRandom.length; i++) {
             System.out.println(nbrArrRandom[i]);
         }
 
+        // [1,2,3,4,5.....49,50}
         int[] numbers = new int[50];
-        for (int i= 1; i < numbers.length;i++) {
-            numbers[i]=i;
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i + 1;
         }
-
+        for(int number: numbers){
+            System.out.println(number);
+        }
 
 //        Scanner myScan = new Scanner(System.in);
 //        int[] nbrArrScan = new int[50];
