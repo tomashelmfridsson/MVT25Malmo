c9e73dca642f9efa5cc91247d6d095318560cdbe
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2025-11-03 11:29:45 +0100

    Uppgift skapa 50 tals array, med kommentarer

--- src/arrays/FemtioNummer.java
+++ src/arrays/FemtioNummer.java
@@ -6,9 +6,11 @@ public class FemtioNummer {
 
     public static void main(String[] args) {
 
+        // Manuellt hårdkodade tal
         int[] nummer = {1, 2, 4, 8, 16, 73, 2, 45645, 234, 4563, 23};  // skriv in 50 tal
         System.out.println(nummer);
 
+        // Dubblerar alla tal kräver long då int är för litet för 2^50 (int klarar bara 2^31)
         long[] nbrArr = new long[50];
         // [plats] = värde
         nbrArr[0] = 1;   // startvärde annars har jag inget att dubbla senare
@@ -20,23 +22,26 @@ public class FemtioNummer {
             System.out.println(nbrArr[i]);
         }
 
+        // 50 slumpvisa tal mellan -99 och +99
         int[] nbrArrRandom = new int[50];
         for (int i = 0; i < nbrArrRandom.length; i++) {
-            nbrArrRandom[i] = (int) ((Math.random() - 0.5) * 200);
+            nbrArrRandom[i] = (int) ((Math.random() - 0.5) * 200); // Math random ger ett tal mellan 0-1
         }
         for (int i = 0; i < nbrArrRandom.length; i++) {
             System.out.println(nbrArrRandom[i]);
         }
 
-        // [1,2,3,4,5.....49,50}
+        // Skapar en talföljd med uppräkning [1,2,3,4,5.....49,50}
         int[] numbers = new int[50];
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = i + 1;
         }
-        for(int number: numbers){
+        for(int number: numbers){       // Ett enklare sätt att göra for "alla" talen i en array
             System.out.println(number);
         }
 
+        // Scanner kommer bli jobbig att köra för en array med 50 tal
+        // **********************************************************
 //        Scanner myScan = new Scanner(System.in);
 //        int[] nbrArrScan = new int[50];
 //        for (int i = 1; i < 50; i++) {
