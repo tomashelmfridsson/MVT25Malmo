7cb16f4bb5fb167ce1b2a697a9ac1950d0b4498d
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2025-11-19 10:27:24 +0100

    Först till 21

---
+++ src/game/FirstTo21.java
@@ -0,0 +1,13 @@
+package game;
+
+//Regler
+// Två spelare
+// Man börjar på 0
+// Man får öka talet med 1 eller 2
+// Den som säger 21 vinner
+
+public class FirstTo21 {
+    public static void main(String[] args) {
+
+    }
+}
