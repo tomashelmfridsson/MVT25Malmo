8b14a83264ccbbc44c954dc237b277035311355c
tomas.helmfridsson <tomas.helmfridsson@sj.se>
2025-11-19 11:49:25 +0100

    Först till 21 exempel

--- src/game/FirstTo21.java
+++ src/game/FirstTo21.java
@@ -1,13 +1,39 @@
 package game;
-
 //Regler
 // Två spelare
 // Man börjar på 0
 // Man får öka talet med 1 eller 2
 // Den som säger 21 vinner
-
+import java.util.Scanner;
 public class FirstTo21 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int currentNumber = 0;
+        System.out.println("Välkommen till spelet! Den som säger 21 vinner.");
+        System.out.println("Du börjar!");
+        while (true) {
+            int playerMove = 0;
+            while (playerMove != 1 && playerMove != 2) {
+                System.out.print("Säg 1 eller 2: ");
+                playerMove = scanner.nextInt();
+                if (playerMove != 1 && playerMove != 2) {
+                    System.out.println("Ogiltigt val, försök igen.");
+                }
+            }
+            currentNumber += playerMove;
+            System.out.println("Du sa: " + currentNumber);
+            if (currentNumber >= 21) {
+                System.out.println("Grattis! Du vann!");
+                break;
+            }
+            int computerMove = ((currentNumber + 1) % 3) == 0 ? 1 : 2;
+            currentNumber += computerMove;
+            System.out.println("Datorn säger: " + currentNumber);
+            if (currentNumber >= 21) {
+                System.out.println("Datorn vann! Bättre lycka nästa gång.");
+                break;
+            }
+        }
+        scanner.close();
     }
 }
